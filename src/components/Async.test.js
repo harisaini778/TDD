37a2a25b60da953d91c1dde08ba0b7a253e0eef1
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-26 23:52:50 +0530

    added a mock to check the test case for fetch method,instead of using real url, this is helpful becuase it helps in avoiding unecessary network calls

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,10 +4,14 @@ import Async from "./Async";
 
 describe("Async component", () => {
     
-    test("renders posts if request succeeds", async() => {
+    test("renders posts if request succeeds", async () => {
+        
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json : async () => [{id:1,title:"sunt aut facere repellat provident occaecati excepturi optio reprehenderit"}],
+        })
         render(<Async />);
         const listItemElements = await screen.findAllByRole("listitem");
-        console.log(listItemElements.length);
         expect(listItemElements).not.toHaveLength(0);
     });
 })
