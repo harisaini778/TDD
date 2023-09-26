6362bdf82e4fd421a450eca6b0737b6a75b5cd70
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-26 16:14:45 +0530

    added a describe method, which creates atest suites, and passed test as the argument

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,8 +1,12 @@
 import { render,screen } from "@testing-library/react";
 import Greetings from "./Greeting";
 
-test("renders hellow world", () => {
+describe("Greeting component", () => {
+  test("renders hellow world", () => {
     render(<Greetings />);
     const helloWorldElement = screen.getByText("Hello World");
     expect(helloWorldElement).toBeInTheDocument();
-});   // test function is globally available we do not need to import it
+});   
+})
+
+  // test function & describe is globally available we do not need to import it
