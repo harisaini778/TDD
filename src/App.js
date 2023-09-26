ebdb6bb0293bad8792ae2292456c74ba28fb5079
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-26 23:18:03 +0530

    added a new code for the Async component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Greetings from './components/Greeting';
 import './App.css';
+import Async from './components/Async';
 
 function App() {
   return (
     <div className="App">
-    <Greetings/>
+      <Greetings />
+      <Async/>
     </div>
   );
 }
